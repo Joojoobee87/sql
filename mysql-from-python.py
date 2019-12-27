ba484d43bcbe6d3d15776b495cfd585f98771c09
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2019-12-27 20:49:53 +0000

    amended mysql-from-python.py spelling error in password

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,23 +1,23 @@
 import os
 import pymysql
 
-#Get username from Cloud9 workspace
-#(modify this variable if running on another environment)
+# Get username from Cloud9 workspace
+# (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
-#connect to the database
+# Connect to the database
 connection = pymysql.connect(host='localhost',
-                            user=username,
-                            passwsord='',
-                            db='Chinook')
-                            
+                             user=username,
+                             password='',
+                             db='Chinook')
+
 try:
-    #run a query
+    # Run a query
     with connection.cursor() as cursor:
         sql = "SELECT * FROM Artist;"
         cursor.execute(sql)
         result = cursor.fetchall()
         print(result)
 finally:
-    #close the connection regardless of whether the above was successful
+    # Close the connection, regardless of whether or not the above was successful
     connection.close()
