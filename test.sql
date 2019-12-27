a72491ce3e98c9c8c139dda093918b13526a6b7c
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2019-12-27 20:39:58 +0000

    removed sql files

--- test.sql
+++ test.sql
@@ -158,4 +158,4 @@ INSERT INTO Track
 VALUES
     ("Another Time, Another Place", 348, 2, 1, "U2", 210000, 1234, 0.99),
     ("The Electric Co.", 348, 2, 1, "U2", 200000, 1234, 0.99),
-    ("Shadows and Tall Trees", 348, 2, 1, "U2", 150000, 1234, 0.99);
+    ("Shadows and Tall Trees", 348, 2, 1, "U2", 150000, 1234, 0.99)
